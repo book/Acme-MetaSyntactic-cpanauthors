c9a54c0abcd6f996348145244a0f32767f381831
Philippe Bruhat (BooK) <book@cpan.org>
2014-05-12 00:32:33 +0200

    protect against empty CPANAuthors categories
    The current source of Acme::CPANAuthors::Dutch is:
    
        use Acme::CPANAuthors::Register (
            '' => '',
        );
    
    which makes it effectively empty.

--- lib/Acme/MetaSyntactic/cpanauthors.pm
+++ lib/Acme/MetaSyntactic/cpanauthors.pm
@@ -3,17 +3,17 @@ package Acme::MetaSyntactic::cpanauthors;
 use strict;
 use Acme::MetaSyntactic::MultiList;
 use Acme::CPANAuthors;
-our @ISA     = qw( Acme::MetaSyntactic::MultiList );
+our @ISA = qw( Acme::MetaSyntactic::MultiList );
 
-__PACKAGE__->init(
-    {   default => ':all',
-        names   => {
-            map {
-                $_ => map { tr/-/_/; $_ } join ' ',
-                    Acme::CPANAuthors->new($_)->id
-                } Acme::CPANAuthors->_list_categories()
-        }
-    }
+my %names = (
+    map {
+        $_ => map { tr/-/_/; $_ } join ' ', Acme::CPANAuthors->new($_)->id
+        } Acme::CPANAuthors->_list_categories()
 );
+
+# protect against empty categories (Acme::CPANAuthors::Dutch)
+delete $names{$_} for grep !$names{$_}, keys %names;
+
+__PACKAGE__->init( { default => ':all', names => \%names } );
 
 1;
